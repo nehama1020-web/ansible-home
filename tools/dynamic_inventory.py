f466fbe356210342694357b7d117d9695ab78cd2
nehama1020-web <nehama1020@gmail.com>
2025-11-25 20:28:20 -0600

    Update dynamic_inventory.py

--- tools/dynamic_inventory.py
+++ tools/dynamic_inventory.py
@@ -1,51 +1,68 @@
-#!/usr/bin/env python3.
+#!/usr/bin/env python3
 import json
+import subprocess
 import sys
 
-HOSTS = {
-    "vm1": {
-        "ansible_host": "10.56.44.183",
-        "ansible_user": "ubuntu",
-        "ansible_ssh_private_key_file": "~/.ssh/id_rsa",
-    },
-    "vm2": {
-        "ansible_host": "10.56.44.29",
-        "ansible_user": "ubuntu",
-        "ansible_ssh_private_key_file": "~/.ssh/id_rsa",
-    },
-    "vm3": {
-        "ansible_host": "10.56.44.246",
-        "ansible_user": "ubuntu",
-        "ansible_ssh_private_key_file": "~/.ssh/id_rsa",
-    },
-}
 
-inventory = {
-    "web": {
-        "hosts": ["vm1"],
-    },
-    "db": {
-        "hosts": ["vm2"],
-    },
-    "galaxy_web": {
-        "hosts": ["vm3"],
-    },
-    "_meta": {
-        "hostvars": HOSTS,
-    },
-}
+# Get all multipass instances as JSON
+def get_multipass_instances():
+    result = subprocess.run(
+        ["multipass", "list", "--format", "json"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    data = json.loads(result.stdout)
+    return data.get("list", [])
 
-def main():
-    # ansible-inventory --list
-    if len(sys.argv) == 2 and sys.argv[1] == "--list":
-        print(json.dumps(inventory))
-    # ansible-inventory --host <hostname>
-    elif len(sys.argv) == 3 and sys.argv[1] == "--host":
-        host = sys.argv[2]
-        print(json.dumps(inventory["_meta"]["hostvars"].get(host, {})))
-    else:
-        # default empty
-        print(json.dumps({}))
+
+def build_inventory():
+    instances = get_multipass_instances()
+
+    inventory = {
+        "web": {"hosts": []},
+        "db": {"hosts": []},
+        "galaxy_web": {"hosts": []},
+        "_meta": {"hostvars": {}},
+    }
+
+    for inst in instances:
+        name = inst["name"]
+        ips = inst.get("ipv4", [])
+        if not ips:
+            continue
+
+        ip = ips[0]
+
+        # Map instance name → Ansible group
+        if name == "hs1":
+            group = "web"
+        elif name == "hs2":
+            group = "db"
+        elif name == "hs3":
+            group = "galaxy_web"
+        else:
+            # Ignore other multipass VMs
+            continue
+
+        inventory[group]["hosts"].append(name)
+
+        inventory["_meta"]["hostvars"][name] = {
+            "ansible_host": ip,
+            "ansible_user": "ubuntu",
+            # adjust this path if your key is somewhere else on the runner
+            "ansible_ssh_private_key_file": "/home/neha/.ssh/ansible_vm",
+        }
+
+    return inventory
+
 
 if __name__ == "__main__":
-    main()
+    # Ansible calls with --list / --host
+    if len(sys.argv) == 2 and sys.argv[1] == "--list":
+        print(json.dumps(build_inventory(), indent=2))
+    elif len(sys.argv) == 2 and sys.argv[1] == "--host":
+        # not needed because we use _meta.hostvars
+        print(json.dumps({}))
+    else:
+        print(json.dumps(build_inventory(), indent=2))
